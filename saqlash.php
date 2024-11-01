71b183cb8b2c5dfe09d0bfa11ff05fa5db4dc17e
karamatullayeva <brightstarmaftuna@gmail.com>
2024-11-01 19:03:23 +0500

    Updated form fields and fixed PHP connection

--- saqlash.php
+++ saqlash.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Students Table</title>
-    <link rel="stylesheet" href="student.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 <body>
 
@@ -21,7 +21,7 @@ if (isset($_POST["submit"])) {
     $course = $_POST['course'];
 
 
-    $conn = new PDO("mysql:host=localhost;dbname=students", "root", "****");
+    $conn = new PDO("mysql:host=localhost;dbname=students", "root", "maftuna2005");
 
     $insertinfo = "INSERT INTO students (fname, lname, age, fakultet, course) VALUES (:fname, :lname, :age, :fakultet, :course)";
 
@@ -35,28 +35,34 @@ if (isset($_POST["submit"])) {
     ]);
 
     $query = $conn->query("SELECT * FROM students");
-    
 
-    echo "<table>
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Age</th>
-                        <th>Faculty</th>
-                        <th>Course</th>
-                    </tr>
-                </thead>
-                <tbody>";
+    echo '<table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">First Name</th>
+                    <th scope="col">Last Name</th>
+                    <th scope="col">Age</th>
+                    <th scope="col">Faculty</th>
+                    <th scope="col">Course</th>
+                </tr>
+            </thead>
+            <tbody>
+    ';
+
+    $counter=1;
 
     foreach ($query as $row) {
-        echo "<tr>
+        echo"<tr>
+                <td>{$counter}</td>
                 <td>{$row['fname']}</td>
                 <td>{$row['lname']}</td>
                 <td>{$row['age']}</td>
                 <td>{$row['fakultet']}</td>
                 <td>{$row['course']}</td>
-            </tr>";
+                </tr>
+            ";
+        $counter++;
     }
 
     echo "</tbody></table>";
--- student.php
+++ student.php
@@ -5,60 +5,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Register form</title>
-    <link rel="stylesheet" href="student.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <form action="saqlash.php" method="post">
-        <div class="heading">
-            <h2>Student Register form</h2>
-        </div>
+    <div class="container">
+        <form action="saqlash.php" method="post">
+            <div class="heading">
+                <h2 class="text-danger text-center">Student Register form</h2>
+            </div>
 
-        <div class="labels">
-            <label for="fname">First name
-                <input type="text" placeholder="Enter First name" id="fname" name="fname">
-            </label>
+            <div class="mb-3">
+                <label for="fname" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="fname" name="fname" aria-describedby="Enter first name">
+                <div id="fname" class="form-text"></div>
+            </div>
 
-            <br><br>
+            <div class="mb-3">
+                <label for="lname" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lname" name="lname">
+            </div>
 
-            <label for="lname">Last name
-                <input type="text" placeholder="Enter Last name" id="lname" name="lname">
-            </label>
-
-            <br><br>
-
-            <label for="age">Age
-                <input type="number" placeholder="Enter age" id="age" name="age">
-            </label>
-
-            <br><br>
-
-            <label for="fakultet">Faculty
-                <select name="fakultet" id="fakultet">
-                    <option value="Dasturlash">Dasturlash</option>
-                    <option value="Huquq">Huquq</option>
-                    <option value="Iqtisod">Iqtisod</option>
-                    <option value="Tibbiyot">Tibbiyot</option>
-                </select>
-                </label>
-
-            <br><br>
-
-            <label for="course">Course
-                <select name="course" id="course">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                </select>
-            </label>
-        </div>
-
-        <br>
-        <button type="submit" name="submit">Submit</button>
+            <div class="mb-3">
+                <label for="age" class="form-label">Age</label>
+                <input type="number" class="form-control" id="age" name="age">
+            </div>
 
 
-    </form>
+            <label for="fakultet" class="form-label">Faculty</label>
+            <select class="form-select" aria-label="Select" name="fakultet">
+                <option selected>Select Faculty:</option>
+                <option value="Programming">Programming</option>
+                <option value="Medicine">Medicine</option>
+                <option value="Law">Law</option>
+                <option value="Finance">Finance</option>
+
+            </select>
+
+            <br>
+
+            <label for="course" class="form-label">Course</label>
+            <select class="form-select" aria-label="Select" name="course">
+                <option selected>Select Course:</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+
+            <br>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>  
+
+        </form>
+    </div>
+
+
 </body>
 
 </html>
